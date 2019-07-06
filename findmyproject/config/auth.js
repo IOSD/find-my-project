1c3218cd3c42efcfa80203c8b07967ac20d6a265
Shreya Prasad <shreyapd06@gmail.com>
2019-07-06 11:54:30 +0530

    Add files via upload
    I've made a Node.js app which consists of a dummy login and registration page along with server and database. Authentication is done using Passport.js

---
+++ findmyproject/config/auth.js
@@ -0,0 +1,18 @@
+module.exports = {
+  ensureAuthenticated: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    req.flash('error_msg', 'Please log in to view that resource');
+    res.redirect('/users/login');
+  },
+  forwardAuthenticated: function (req, res, next) {
+    if (!req.isAuthenticated()) {
+      next();
+    } else {
+      res.render('dashboard',{
+        user:req.user
+      })
+    }
+  }
+};
---
+++ findmyproject/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://sGurkaran:@Rs3ed2hjm3@cluster0-vcai1.mongodb.net/test?retryWrites=true&w=majority'
+};
